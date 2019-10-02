fda92608f57116ab5d8aa957c053fe9962a30f29
guayGonzalez <y.gonzalez.dev@lacortedelosbuhos.com>
2019-10-01 23:27:53 -0500

    responsiveness int

---
+++ Scripts/Animate.js
@@ -0,0 +1,20 @@
+/* $(document).ready(() =>{
+  const elements;
+  const windowHeight;
+
+  elements = document.querySelectorAll('.hidden');
+  windowHeight = window.innerHeight;
+
+});
+
+$(document).onScroll(() =>{
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        var positionFromTop = elements[i].getBoundingClientRect().top;
+
+        if (positionFromTop - windowHeight <= 0) {
+          element.classList.add('fade-in-element');
+          element.classList.remove('hidden');
+        }
+      }
+});*/
