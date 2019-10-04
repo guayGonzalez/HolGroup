603a4accdd5d8aadaa7ccc6d59cd60cbd83fa065
Yensy Gonzalez <y.gonzalez.dev@lacortedelosbuhos.com>
2019-10-03 23:04:29 -0500

    full size animations complete

--- Scripts/Animate.js
+++ Scripts/Animate.js
@@ -1,11 +1,27 @@
 /* eslint-disable require-jsdoc */
 $(document).ready(()=>{
+  window.innerWidth > 600 ? set() : setMobile();
+});
+
+function setMobile() {
+  animateSingle('.Header img', 'Appear', 'mainHidden', 500);
+
+  animateSingle('.mainTitle', 'leftAppear', 'mainHidden', 2000);
+
+  animateSingle('.menu', 'topAppear', 'mainHidden', 4000);
+
+  animateSingle('.logo', 'Appear', 'mainHidden', 5000);
+}
+
+function set() {
   animateSingle('.Header img', 'mainAppear', 'mainHidden', 500);
 
   animateSingle('.mainTitle', 'leftAppear', 'mainHidden', 4000);
 
-  animateGroup('.menu ul li', 'x', 'x', 'x');
-});
+  animateSingle('.menu', 'topAppear', 'mainHidden', 6000);
+
+  animateSingle('.logo', 'Appear', 'mainHidden', 7000);
+}
 
 function animateSingle(obj, sc, rc, t) {
   setTimeout(() =>{
@@ -13,10 +29,4 @@ function animateSingle(obj, sc, rc, t) {
   }, t);
 }
 
-function animateGroup(objs, sc, rc, t) {
-  const objects = $(objs);
 
-  for (let i = 0; i<objects.length; i++) {
-    console.log(objects[i]);
-  }
-}
---
+++ Scripts/scroll.js
@@ -0,0 +1,107 @@
+/* eslint-disable new-cap */
+/* eslint-disable require-jsdoc */
+const controller = new ScrollMagic.Controller();
+
+window.innerWidth > 600 ? Big() : Small();
+
+function Big() {
+  // News Section
+
+  const tl = new TimelineMax({onUpdate: updatePercentage});
+
+  tl.from('.News', .5, {x: '-100%', opacity: 0});
+
+  new ScrollMagic.Scene({
+    triggerElement: '.Header',
+    triggerHook: 0,
+    duration: '50%',
+  })
+      .setTween(tl)
+      .addTo(controller);
+
+  // Experience Section
+
+  const tl2 = new TimelineMax();
+
+  tl2.from('#expTitle', 1, {opacity: 0});
+  tl2.from('#expDesc', 1, {opacity: 0}, .5);
+
+  new ScrollMagic.Scene({
+    triggerElement: '.Titles',
+    triggerHook: 0.75,
+  })
+      .setTween(tl2)
+      .addTo(controller);
+
+  // Us Section
+
+  const tl3 = new TimelineMax();
+
+  tl3.from('#usTitle', 1, {opacity: 0});
+
+  new ScrollMagic.Scene({
+    triggerElement: '.About',
+    triggerHook: 0.75,
+  })
+      .setTween(tl3)
+      .addTo(controller);
+
+  const tl4 = new TimelineMax({onUpdate: updatePercentage});
+
+  tl4.from('.About .images .colorSec', 1, {x: '-100%'});
+
+  new ScrollMagic.Scene({
+    triggerElement: '.About',
+    triggerHook: 0.70,
+    duration: '20%',
+  })
+      .setTween(tl4)
+      .addTo(controller);
+
+  const tl5 = new TimelineMax();
+
+  tl5.from('.About .images img', 1, {opacity: 0});
+
+  new ScrollMagic.Scene({
+    triggerElement: '#usDesc',
+    triggerHook: 0.70,
+  })
+      .setTween(tl5)
+      .addTo(controller);
+
+  // Contact Section
+
+  const tl6 = new TimelineMax();
+
+  tl6.from('.Contact .main h2', 1, {opacity: 0});
+
+  new ScrollMagic.Scene({
+    triggerElement: '.Contact',
+    triggerHook: 0.75,
+  })
+      .setTween(tl6)
+      .addTo(controller);
+
+  const tl7 = new TimelineMax({onUpdate: updatePercentage});
+
+  tl7.from('.contForm', 1, {width: 0});
+
+  new ScrollMagic.Scene({
+    triggerElement: '.Contact',
+    triggerHook: 0.60,
+    duration: '30%',
+  })
+      .setTween(tl7)
+      .addTo(controller);
+}
+
+function Small() {
+
+
+}
+
+
+function updatePercentage() {
+  tl.progress();
+}
+
