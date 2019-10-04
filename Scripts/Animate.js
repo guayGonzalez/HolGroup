a9a13b7e8880a446cb751ab1f230c767d2cd57e8
Yensy Gonzalez <y.gonzalez.dev@lacortedelosbuhos.com>
2019-10-03 19:43:03 -0500

    k

--- Scripts/Animate.js
+++ Scripts/Animate.js
@@ -1,20 +1,35 @@
-/* $(document).ready(() =>{
-  const elements;
-  const windowHeight;
+/* eslint-disable require-jsdoc */
+let elements;
+let windowHeight;
 
+function init() {
   elements = document.querySelectorAll('.hidden');
+
   windowHeight = window.innerHeight;
+}
 
-});
+function checkPosition() {
+  const news = document.getElementsByClassName('News');
 
-$(document).onScroll(() =>{
-    for (var i = 0; i < elements.length; i++) {
-        var element = elements[i];
-        var positionFromTop = elements[i].getBoundingClientRect().top;
+  if (news[0].getBoundingClientRect().top - windowHeight <= -250) {
+    news[0].classList.add('fade-InLeft');
+    news[0].classList.remove('hiddenLeft');
+  }
 
-        if (positionFromTop - windowHeight <= 0) {
-          element.classList.add('fade-in-element');
-          element.classList.remove('hidden');
-        }
-      }
-});*/
+  for (let i = 0; i < elements.length; i++) {
+    const element = elements[i];
+
+    const posFromTop = elements[i].getBoundingClientRect().top;
+
+    if (posFromTop - windowHeight <= 0) {
+      element.classList.add('fade-In');
+      element.classList.remove('hidden');
+    }
+  }
+}
+
+window.addEventListener('scroll', checkPosition);
+
+init();
+checkPosition();
+
